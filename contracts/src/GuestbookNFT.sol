393ffb248dbe9850450180112f9708765544bf68
wojtekwtf <wojciechkulikowski@icloud.com>
2023-08-05 22:24:57 +0100

    new contract

--- contracts/src/GuestbookNFT.sol
+++ contracts/src/GuestbookNFT.sol
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
+import "openzeppelin-contracts/contracts/Counters.sol";
 
 contract BaseGuestbook is ERC721, Ownable {
     using Counters for Counters.Counter;
+
+    mapping(uint256 => string) private _entries;
 
     Counters.Counter private _tokenIdCounter;
 
@@ -16,9 +18,24 @@ contract BaseGuestbook is ERC721, Ownable {
         return "https://baseguestbook.vercel.app/";
     }
 
-    function safeMint(address to) public onlyOwner {
+    function safeMint(address to, string memory entry) public {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
+        _entries[tokenId] = entry;
         _safeMint(to, tokenId);
+    }
+
+    function tokenURI(uint256 tokenId) public {
+        // should return the url which consist of baseURI + tokenId + entry
+        // e.g. https://baseguestbook.vercel.app/0?entry=Hello%20World
+        return
+            string(
+                abi.encodePacked(
+                    _baseURI(),
+                    tokenId,
+                    "?entry=",
+                    _entries[tokenId]
+                )
+            );
     }
 }
